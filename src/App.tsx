1cd4d98d072990df640d5da073ee5f552bc4d120
Daniel Lödermann <d.loedermann@googlemail.com>
2022-11-13 20:14:06 +0100

    add mantine ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "@mantine/core";
 
 function App() {
   return (
@@ -18,6 +19,10 @@ function App() {
         >
           Learn React
         </a>
+        <Button>Click me!</Button>
+        <Button>Click me!</Button>
+        <Button>Click me!</Button>
+        <Button>Click me!</Button>
       </header>
     </div>
   );
---
+++ src/components/routerlist.ts
@@ -0,0 +1,1 @@
+import { MantineProvider } from "@mantine/core";
